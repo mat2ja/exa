26e420fbea2457f00ecf1245faf3ca86407118b0
Matija Osrečki <matija.osrecki@gmail.com>
2023-01-25 19:18:38 +0100

    Remove alias

--- exa.plugin.zsh
+++ exa.plugin.zsh
@@ -1,6 +1,5 @@
 alias ls="exa -F --icons --group-directories-first"
-alias l="exa -lF --icons --group-directories-first --git --no-user"
-alias ll="exa -lFa --icons --group-directories-first --git --no-user"
+alias l="exa -lFa --icons --group-directories-first --git --no-user"
 alias ld="exa -lFa --icons --group-directories-first --git --only-dirs"
 alias la="exa -lFaa --icons --group-directories-first --git"
 alias llg="exa -lFaa --icons --group-directories-first --git --grid"
